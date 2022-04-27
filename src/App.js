a80e79d4bb7f11cd829c9457adaea61055fde47a
Todd <hello@toddaas.com>
2022-04-28 08:47:41 +1000

    setup form component and horse api

--- src/App.js
+++ src/App.js
@@ -1,37 +1,75 @@
+//import { findAllByDisplayValue } from '@testing-library/dom';
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <Tracks />
+        <Form />
     </div>
   );
 }
 
-class Tracks extends React.Component {
+class Form extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tracks: []
+      tracks: [],
+      isLoading: false,
     }
   }
 
-  componentDidMount() {
-    // fetch API curl -X GET "http://api.horseapi.com/tracks" -H  "accept: application/json" -H  "Authorization: <insert api key>"
-
+  handleSelect() {
 
   }
-  
+
+  componentDidMount() {
+    // fetch API curl -X "http://api.horseapi.com/tracks" -H  "accept: application/json" -H  "Authorization: <insert api key>"
+
+    this.setState({ isLoading: true });
+    
+    fetch ('http://api.horseapi.com/tracks', { 
+        headers: {
+          method: 'GET',
+          accept: 'application/json',
+          Authorization: process.env.REACT_APP_ENV_HORSEAPI_KEY
+        },
+      })
+      .then(response => response.json())
+      .then(data => this.setState( {
+        tracks: data,
+        isLoading: false
+      }));
+
+  }
+  // need to read up on my === vs ==? promises, map function, in general ES6 post saved to pocket
   render () {
+    //const { tracks, isLoading} = this.state; - need to review using this syntax
+
+    if (this.state.isLoading) {
+      return <p>Loading...</p>
+    }
     return (
       <div>
-        <form>
           <label for="tracks">Select track: </label>
             <select id="tracks" name="tracks">
-              
+            {this.state.tracks.map(track =>
+              <option value={track.id}>{track.name}</option>
+              )}
           </select>
-        </form>
+          <label for="raceNum"> Select race #: </label>
+          <select id="raceNum" name="raceNum">
+          {this.state.tracks.map((track,index)=>{
+            if (index === 0) {
+              let races = [];
+              for (let i = 1; i <= track.raceCount; i++) {
+                races.push(<option value={i}>{i}</option>);
+              }
+              return races
+          }
+          return null
+          })}
+          </select>
       </div>
     );
   }
